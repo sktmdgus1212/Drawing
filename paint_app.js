36d3385fdad9fa27d2a3c1e6f8418bc691142774
sktmdgus1212 <43513001+sktmdgus1212@users.noreply.github.com>
2021-10-26 01:24:24 +0900

    ver2
    서버로 저장은 ok / 서버로부터 불러오지못함

--- paint_app.js
+++ paint_app.js
@@ -1,18 +1,19 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
-const cte = canvas.getContext("2d");;
+const cte = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
 const eraser = document.getElementById("jsEraser");
-
+const xy = [];
 const INTITAL_COLOR = "#2c2c2c";
+const webSocket = new WebSocket("ws://192.168.0.23:30001/websocket");
 canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
 canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
 
 ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.fillStyle = INTITAL_COLOR;
 ctx.strokeStyle = INTITAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -33,7 +34,8 @@ function onMouseMove(event){
   //console.log(event);
   const x= event.offsetX;
   const y= event.offsetY;
-//  console.log(x, y);
+  xy[0] = x;
+  xy[1] = y;
   if(!painting){
     ctx.beginPath();
     ctx.moveTo(x,y);
@@ -41,7 +43,18 @@ function onMouseMove(event){
   else{
     ctx.lineTo(x, y);
     ctx.stroke();
+
+    if(webSocket.readyState === webSocket.OPEN){
+      // 연결 상태 확인
+       webSocket.send(`${xy}`);
+       console.log(`xxxx:` +x, y);
+       //console.log(`xy: ${xy}`);
+       // 웹소켓 서버에게 메시지 전송
+     }
+       else{ alert("연결된 웹소켓 서버가 없습니다.");
+     }
   }
+
 }
 
 function handleColorClick(event){
@@ -122,6 +135,7 @@ if(canvas){
   canvas.addEventListener("click", handleCanvasClick);
   canvas.addEventListener("contextmenu", handleCM);
   canvas.addEventListener("click", handleEraserClick);
+  canvas.addEventListener("mousemove", websocketmousemove);
 }
 
 //console.log(Array.from(colors));
@@ -144,3 +158,66 @@ if(saveBtn){
 if(eraser){
   eraser.addEventListener("click", handleeraserbuttonClick);
 }
+
+
+
+webSocket.onopen = ()=>{
+  console.log("웹소켓서버와 연결 성공");
+};
+// 2-2) 메세지 수신 이벤트 처리
+webSocket.onmessage = function (event) {
+  //console.log(`서버 웹소켓에게 받은 데이터: ${event.data}`);
+  const event_xy = event.data.split(",");
+  const event_x = event_xy[0];
+  const event_y = event_xy[1];
+
+  websocketmousemove(event_x, event_y);
+  /*eraser.innerText = event_x;
+  console.log(event_x, event_y);
+  if(!painting){
+    ctx.beginPath();
+    ctx.moveTo(event_x,event_y);
+  }
+  else{
+    ctx.lineTo(event_x,event_y);
+    ctx.stroke();
+      console.log('websocket:'+event_x,event_y);
+  }*/
+}
+
+function websocketmousemove(x, y){
+  btn_send.innerText = count;
+  console.log('websocket:'+x,y);
+  if(!painting){
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+  }
+  else{
+    ctx.lineTo(x,y);
+    ctx.stroke();
+
+  }
+}
+// 2-3) 연결 종료 이벤트 처리
+webSocket.onclose = function(){
+  console.log("서버 웹소켓 연결 종료");
+}
+// 2-4) 에러 발생 이벤트 처리
+webSocket.onerror = function(event){
+  console.log(event)
+}
+
+
+let count = 1;
+document.getElementById("btn_send").onclick = function(){
+  if(webSocket.readyState === webSocket.OPEN){
+    // 연결 상태 확인
+    webSocket.send(`증가하는 숫자를 보냅니다 => ${count}`);
+    // 웹소켓 서버에게 메시지 전송
+    count++;
+    // 보낼때마다 숫자를 1씩 증가
+  }
+    else{
+      alert("연결된 웹소켓 서버가 없습니다.");
+    }
+  }
--- paint_main.js
+++ paint_main.js
@@ -1,17 +1,55 @@
-var http = require('http');
-var fs = require('fs');
-var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/paint_index.html';
-    }
-    if(request.url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+const path = require("path"); // HTTP 서버(express) 생성 및 구동
+// 1. express 객체 생성
+const express = require('express');
+const app = express();
+let num = 1;
+let usernum = 0;
+// 2. "/" 경로 라우팅 처리
+/*app.use("/", (req, res)=>{
+  res.sendFile(path.join(__dirname, './index.html'));
+
+// index.html 파일 응답
+})*/
+app.use(express.static(path.join(__dirname)));
+// 3. 30001 port에서 서버 구동
+const HTTPServer = app.listen(30001, '192.168.0.23', ()=>{
+  console.log("Server is open at port:30001");
+});
+
+const wsModule = require('ws');
+// 2. WebSocket 서버 생성/구동
+const webSocketServer = new wsModule.Server(
+  {
+    server: HTTPServer,
+    // WebSocket서버에 연결할 HTTP서버를 지정한다.
+    //port: 30002 // WebSocket연결에 사용할 port를 지정한다(생략시, http서버와 동일한 port 공유 사용)
+  }
+);
+
+  // connection(클라이언트 연결) 이벤트 처리
+  webSocketServer.on('connection', (ws, request)=>{
+    // 1) 연결 클라이언트 IP 취득
+    const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+    usernum = num;
+    console.log(`새로운 클라이언트[${ip}] 접속(${num}번)`);
+    num++;
+    // 2) 클라이언트에게 메시지 전송
+    if(ws.readyState === ws.OPEN){
+  // 연결 여부 체크
+  ws.send(`클라이언트[${ip}] 접속을 환영합니다 from 서버`);
+  // 데이터 전송
+  //ws.send(`클라이언트[ss]에게 전송한 메세지 : ss`);
+}
+   // 3) 클라이언트로부터 메시지 수신 이벤트 처리
+   ws.on('message', (msg)=>{ console.log(`클라이언트[${ip}](${usernum}번)에게 수신한 메시지 : ${msg}`);
+
+   //ws.send('메시지 잘 받았습니다! from 서버')
+   ws.send(`${usernum}`);
+ })
+   // 4) 에러 처러
+   ws.on('error', (error)=>{ console.log(`클라이언트[${ip}] 연결 에러발생 : ${error}`); })
+   // 5) 연결 종료 이벤트 처리
+   ws.on('close', ()=>{ console.log(`클라이언트[${ip}] 웹소켓 연결 종료`); })
+
 
 });
-app.listen(4000);
